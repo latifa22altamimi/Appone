b5cbf4c91b2b32932e8ea30b48f3856135238d5f
latifa22altamimi <latifah.maltamimi@gmail.com>
2023-03-29 12:23:18 +0300

    up

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+Button b;
+EditText e;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        b= (Button)findViewById(R.id.button);
+        e=(EditText) findViewById(R.id.editTextTextPersonName);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s = e.getText().toString();
+                Intent intent = new Intent(getApplicationContext(), secondActivity.class);
+                intent.putExtra("editTextTextPersonName", s);
+                startActivity(intent);
+
+            }
+        });
+
+
+    };
 }
